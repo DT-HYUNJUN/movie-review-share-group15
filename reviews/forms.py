a4824fa2c40ce3fd0d28e7f66c25ee947fcc4c6a
Park <angle307@naver.com>
2023-04-13 13:58:45 +0900

    [ADD] modeling comment

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Review
+from .models import Review, Comment
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ('title', 'content', 'movie')
+        fields = ('title', 'content', 'movie', 'image',)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
--- reviews/models.py
+++ reviews/models.py
@@ -8,3 +8,10 @@ class Review(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField(null=True)
     movie = models.CharField(max_length=30)
+    image = models.ImageField(blank=True, upload_to='%Y/%m/%d/')
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)

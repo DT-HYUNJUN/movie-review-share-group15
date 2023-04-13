e23632e20110e2a470a3c683a10667d655328675
jeonseongjae98 <jeonseongjae@naver.com>
2023-04-13 13:45:40 +0900

    reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    content = models.TextField(null=True)
+    movie = models.CharField(max_length=30)

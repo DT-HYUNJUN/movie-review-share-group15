57d6f3c491f9eebd7508952bfdd4bfe848fa40b1
Hanui Jang <hany0147@naver.com>
2023-04-13 15:45:15 +0900

    add create

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review, Comment
+from .forms import ReviewForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'reviews/index.html')
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('reviews:index') # 수정 필요
+    else:
+        form = ReviewForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'reviews/create.html', context)

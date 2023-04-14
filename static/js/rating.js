720a2c9c86f3ed5015ab6fadab329bd593ce1bc0
Park <angle307@naver.com>
2023-04-14 16:13:21 +0900

    [UPDATE] navbar

--- static/js/rating.js
+++ static/js/rating.js
@@ -1,8 +1,6 @@
 const ratings = document.querySelectorAll('#rating')
 
 ratings.forEach(rating => {
-  
-  console.log(rating.textContent)
   if (rating.textContent > 60) {
     rating.classList.add('bg-success')
   } else if (rating.textContent > 40) {
